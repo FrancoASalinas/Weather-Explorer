befff46355a35355281c721faacb33e18a8408c0
FrancoSMiño <frankow222@gmail.com>
2024-07-21 21:52:26 -0300

    fix: map zoom and width.

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -8,21 +8,20 @@ function InteractiveMap() {
 
   useEffect(() => {
     if (!isInitialized) {
-      const map = L.map('map', { center: [0, 0], zoom: 1 });
+      const map = L.map('map', { center: [15, 15], zoom: 3 });
 
-      map &&
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          tileSize: 212,
+          maxZoom: 9,
           attribution:
             '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         }).addTo(map);
 
-      map &&
         L.tileLayer(
           `https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${API_KEY}`,
           { maxZoom: 9 }
         ).addTo(map);
+
+        
 
       setIsInitialized(true);
     }
