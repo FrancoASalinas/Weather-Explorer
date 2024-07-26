9f28e72fed01baca59a8ad727fc72ec6b91f4c7b
FrancoSMiño <frankow222@gmail.com>
2024-07-26 11:09:14 -0300

    feat: custom radio buttons.

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -22,24 +22,43 @@ function SidePanel({
         className='side-panel__toggle'
         onClick={() => setIsToggle(prev => !prev)}
       >
-        <svg className={`toggle${isToggle ? '--active' : ''}`} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'>
+        <svg
+          className={`toggle${isToggle ? '--active' : ''}`}
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 320 512'
+        >
           <path d='M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z' />
         </svg>
       </button>
       {layers.map(({ name }) => (
-        <label htmlFor={name} className='side-panel__select-layer'>
-          <input
-            type='radio'
-            id={name}
-            name='layer'
-            checked={layer.name === name}
-            onChange={onChange}
-            value={name}
-          ></input>
-          <span className='side-panel__select-layer__layer-name'>{name}</span>
-        </label>
+        <CustomRadioInput isChecked={layer.name === name} name={name} onChange={onChange} />
       ))}
     </div>
+  );
+}
+
+function CustomRadioInput({
+  name,
+  isChecked,
+  onChange,
+}: {
+  name: string;
+  isChecked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  return (
+    <label htmlFor={name} className='custom-label'>
+      <input
+      className='radio-button'
+        type='radio'
+        id={name}
+        checked={isChecked}
+        onChange={onChange}
+        value={name}
+      ></input>
+      <span className='custom-radio-button'></span>
+      <span className='custom-label__name'>{name}</span>
+    </label>
   );
 }
 
