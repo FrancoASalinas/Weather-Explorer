42a1f33a4530a33a7f4d2bd68d7afa705b915813
FrancoSMiño <frankow222@gmail.com>
2024-08-21 11:15:00 -0300

    fix: incorrect forecast cards' date.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,6 @@
 function formatDate(date: string) {
     const dateObject = new Date(date);
-    return `${dateObject.getDate()}/${dateObject.getMonth() + 1}`
+    return `${dateObject.getUTCDate()}/${dateObject.getMonth() + 1}`
 }
 
 export default formatDate;
