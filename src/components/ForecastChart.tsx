340d4a9aee9e8b8b13a332aefe949b44ccc5fc3b
FrancoSMiño <frankow222@gmail.com>
2024-09-17 20:43:48 -0300

    fix: lack of responsiveness on the chart.

--- src/components/ForecastChart.tsx
+++ src/components/ForecastChart.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { Forecast } from 'src/utils/transformForecastData';
 import {
   LineChart,
@@ -12,7 +12,23 @@ import {
 } from 'recharts';
 import { DateTime } from 'luxon';
 
-function ForecastChart({ forecastData, axies }: { forecastData: Forecast, axies: boolean}) {
+function ForecastChart({ forecastData }: { forecastData: Forecast }) {
+  const [isChartWide, setIsChartWide] = useState(false);
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function activateAxies() {
+      if (chartRef.current) {
+        chartRef.current.clientWidth > 900 ? setIsChartWide(true) : setIsChartWide(false);
+      }
+    }
+
+    window.addEventListener('resize', activateAxies);
+    activateAxies();
+
+    return () => window.removeEventListener('resize', activateAxies);
+  }, []);
+
   const {
     hours,
     precipitation_probability,
@@ -20,7 +36,6 @@ function ForecastChart({ forecastData, axies }: { forecastData: Forecast, axies:
     wind_direction,
     wind_speed,
   } = forecastData.hourly;
-
 
   const data = useMemo(
     () =>
@@ -35,80 +50,82 @@ function ForecastChart({ forecastData, axies }: { forecastData: Forecast, axies:
   );
 
   return (
-    <ResponsiveContainer>
-      <LineChart data={data}>
-        <Line
-          dataKey='precipitation'
-          name='Precipitation Probability'
-          yAxisId={2}
-          type={'monotone'}
-          stroke='#aaf'
-          dot={false}
-        />
-        <Line
-          dataKey='temperature'
-          name='Temperature'
-          yAxisId={1}
-          type={'monotone'}
-          stroke='#b24333'
-          dot={false}
-        />
-        <Line
-          dataKey='wind_direction'
-          name='Wind Direction'
-          type={'monotone'}
-          stroke='#2aaf04'
-          dot={false}
-          yAxisId={3}
-        />
-        <Line
-          dataKey='wind_speed'
-          name='Wind Speed'
-          type={'monotone'}
-          stroke='#5435af'
-          dot={false}
-          yAxisId={0}
-        />
-        {axies && (
-          <>
-            <YAxis
-              yAxisId={2}
-              width={80}
-              label={{ value: 'Precipitation Probability %', angle: -90 }}
-              dataKey={'precipitation'}
-            />
-            <YAxis
-              yAxisId={1}
-              width={80}
-              label={{ value: 'Temperature ºC', angle: -90 }}
-              dataKey={'temperature'}
-            />
-            <YAxis
-              yAxisId={3}
-              width={80}
-              label={{ value: 'Wind Direction º', angle: -90 }}
-              dataKey={'wind_direction'}
-            />
-            <YAxis
-              yAxisId={0}
-              width={80}
-              label={{ value: 'Wind Speed km/h', angle: -90 }}
-              dataKey={'wind_speed'}
-            />
-          </>
-        )}
-        <Legend />
-        <CartesianGrid vertical={false} />
-        <XAxis dataKey='name' />
-        <Tooltip
-          contentStyle={{
-            backgroundColor: '#1d2e37',
-            border: 'none',
-            borderRadius: 10,
-          }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <div ref={chartRef} className='chart-wrapper'>
+      <ResponsiveContainer>
+        <LineChart data={data}>
+          <Line
+            dataKey='precipitation'
+            name='Precipitation Probability %'
+            yAxisId={2}
+            type={'monotone'}
+            stroke='#aaf'
+            dot={false}
+          />
+          <Line
+            dataKey='temperature'
+            name='Temperature ºC'
+            yAxisId={1}
+            type={'monotone'}
+            stroke='#b24333'
+            dot={false}
+          />
+          <Line
+            dataKey='wind_direction'
+            name='Wind Direction º'
+            type={'monotone'}
+            stroke='#2aaf04'
+            dot={false}
+            yAxisId={3}
+          />
+          <Line
+            dataKey='wind_speed'
+            name='Wind Speed km/h'
+            type={'monotone'}
+            stroke='#5435af'
+            dot={false}
+            yAxisId={0}
+          />
+          {isChartWide && (
+            <>
+              <YAxis
+                yAxisId={2}
+                width={80}
+                label={{ value: 'Precipitation Probability %', angle: -90 }}
+                dataKey={'precipitation'}
+              />
+              <YAxis
+                yAxisId={1}
+                width={80}
+                label={{ value: 'Temperature ºC', angle: -90 }}
+                dataKey={'temperature'}
+              />
+              <YAxis
+                yAxisId={3}
+                width={80}
+                label={{ value: 'Wind Direction º', angle: -90 }}
+                dataKey={'wind_direction'}
+              />
+              <YAxis
+                yAxisId={0}
+                width={80}
+                label={{ value: 'Wind Speed km/h', angle: -90 }}
+                dataKey={'wind_speed'}
+              />
+            </>
+          )}
+          <Legend />
+          <CartesianGrid vertical={false} />
+          <XAxis dataKey='name' />
+          <Tooltip
+            contentStyle={{
+              backgroundColor: '#1d2e37',
+              border: 'none',
+              borderRadius: 10,
+            }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
 
--- src/components/LocationWeather.tsx
+++ src/components/LocationWeather.tsx
@@ -5,7 +5,7 @@ import { Forecast } from 'src/utils/transformForecastData';
 import { ForecastCardData } from 'src/types';
 import useForecast from 'src/utils/useForecast';
 import Weather from './Weather';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import ForecastChart from './ForecastChart';
 import Angle from 'src/assets/icons/right-angle.svg?react';
 
@@ -17,15 +17,7 @@ export default function LocationWeather({
   const [forecast, setForecast, error] = useForecast(coords);
   const [isLoadingImage, setIsLoadingImage] = useState(true);
   const [isChart, setIsChart] = useState(false);
-  const [isChartWide, setIsChartWide] = useState(false);
   const isLoading = !forecast && !error;
-  const chartRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (chartRef.current) {
-      chartRef.current.clientWidth > 900 && setIsChartWide(true);
-    }
-  }, []);
 
   function handleCardClick(cardData: ForecastCardData) {
     setForecast(cardData);
@@ -39,10 +31,7 @@ export default function LocationWeather({
       {(isLoading || isLoadingImage) && <LoadingIndicator />}
       {!isLoading && (
         <>
-          <div
-            className={`chart ${isChart ? 'chart--active' : ''}`}
-            ref={chartRef}
-          >
+          <div className={`chart ${isChart ? 'chart--active' : ''}`}>
             <button
               onClick={() => setIsChart(prev => !prev)}
               className={`chart__button ${
@@ -52,10 +41,7 @@ export default function LocationWeather({
             >
               <Angle className='chart__button__icon' />
             </button>
-            <ForecastChart
-              forecastData={forecast as Forecast}
-              axies={isChartWide}
-            />
+            <ForecastChart forecastData={forecast as Forecast} />
           </div>
           <Weather
             weatherData={forecast?.current as Forecast['current']}
